4f7487fd88421ae906415c66afa5cd17050ecd9a
vaibhavahbiav <mahawarv97@gmail.com>
2025-03-28 16:17:50 +0530

    fixing dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,91 +2,91 @@ import React, { useEffect, useState } from "react";
 import { Link, Outlet, useNavigate, useLocation } from "react-router-dom";
 
 const Dashboard = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [isSidebarOpen, setSidebarOpen] = useState(true);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [isSidebarOpen, setSidebarOpen] = useState(true);
 
-  const handleLogout = () => {
-    localStorage.removeItem("authToken");
-    localStorage.removeItem("userEmail");
-    navigate("/");
-  };
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!isSidebarOpen);
-  };
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth < 770) {
-        setSidebarOpen(false);
-      } else {
-        setSidebarOpen(true);
-      }
+    const handleLogout = () => {
+        localStorage.removeItem("authToken");
+        localStorage.removeItem("userEmail");
+        navigate("/");
     };
 
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
+    const toggleSidebar = () => {
+        setSidebarOpen(!isSidebarOpen);
     };
-  }, []);
 
-  const isActiveRoute = (route) => location.pathname.includes(route);
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 770) {
+                setSidebarOpen(false);
+            } else {
+                setSidebarOpen(true);
+            }
+        };
 
-  return (
-    <div className="min-h-screen bg-gray-100 flex shadow-lg shadow-orange-500 rounded-lg">
-      <div
-        className={`${isSidebarOpen ? "w-64" : "w-16"
-          } bg-orange-500 text-gray-200 transition-all duration-300 p-4 fixed h-full flex flex-col`}
-      >
-        <div className="flex justify-around items-center mb-8">
-          <h1 className={`${isSidebarOpen ? "block" : "hidden"} text-xl font-bold`}>
-            {isSidebarOpen ? 'EmployWise Dashboard' : 'EmployWise'}
-          </h1>
-          <button
-            className="text-gray-200 md:hidden"
-            onClick={toggleSidebar}
-          >
-            {isSidebarOpen ? "Close" : "Open"}
-          </button>
+        handleResize();
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    const isActiveRoute = (route) => location.pathname.includes(route);
+
+    return (
+        <div className="min-h-screen bg-gray-100 flex shadow-lg shadow-orange-500 rounded-lg">
+            <div
+                className={`${isSidebarOpen ? "w-64" : "w-16"
+                    } bg-orange-500 text-gray-200 transition-all duration-300 p-4 fixed h-full flex flex-col z-50`}
+            >
+                <div className="flex justify-around items-center mb-8">
+                    <h1 className={`${isSidebarOpen ? "block" : "hidden"} text-xl font-bold`}>
+                        {isSidebarOpen ? 'EmployWise Dashboard' : 'EmployWise'}
+                    </h1>
+                    <button
+                        className="text-gray-200 md:hidden"
+                        onClick={toggleSidebar}
+                    >
+                        {isSidebarOpen ? "Close" : "Open"}
+                    </button>
+                </div>
+
+                <Link
+                    to="info"
+                    className={`${isSidebarOpen ? "block" : "text-center"
+                        } p-2 mb-4 text-gray-200 hover:bg-orange-400 rounded-lg group relative overflow-clip ${isActiveRoute("/info") ? "bg-orange-400 font-bold" : ""}`}
+                >
+                    <span className="before:absolute before:content-[''] before:w-full before:h-[2px] before:bg-gray-200 before:bottom-2 before:-left-[100%] group-hover:before:left-[100%] group-hover:before:transition-all group-hover:before:duration-1000">
+                        {isSidebarOpen ? "Info" : "I"}
+                    </span>
+                </Link>
+
+                <Link
+                    to="users"
+                    className={`${isSidebarOpen ? "block" : "text-center"
+                        } p-2 mb-4 text-gray-200 hover:bg-orange-400 rounded-lg group relative overflow-clip ${isActiveRoute("/users") ? "bg-orange-400 font-bold" : ""}`}
+                >
+                    <span className="before:absolute before:content-[''] before:w-full before:h-[2px] before:bg-gray-200 before:bottom-2 before:-left-[100%] group-hover:before:left-[100%] group-hover:before:transition-all group-hover:before:duration-1000">
+                        {isSidebarOpen ? "Users" : "U"}
+                    </span>
+                </Link>
+
+                <button
+                    onClick={handleLogout}
+                    className={`${isSidebarOpen ? "block" : "text-center"
+                        } p-2 bg-gray-100 text-gray-800 hover:bg-red-500 hover:text-gray-200 rounded-lg`}
+                >
+                    {isSidebarOpen ? "Logout" : "L"}
+                </button>
+            </div>
+
+            <div className="flex-1 ml-16 md:ml-64 p-6">
+                <Outlet />
+            </div>
         </div>
-
-        <Link
-          to="info"
-          className={`${isSidebarOpen ? "block" : "text-center"
-            } p-2 mb-4 text-gray-200 hover:bg-orange-400 rounded-lg group relative overflow-clip ${isActiveRoute("/info") ? "bg-orange-400 font-bold" : ""}`}
-        >
-          <span className="before:absolute before:content-[''] before:w-full before:h-[2px] before:bg-gray-200 before:bottom-2 before:-left-[100%] group-hover:before:left-[100%] group-hover:before:transition-all group-hover:before:duration-1000">
-            {isSidebarOpen ? "Info" : "I"}
-          </span>
-        </Link>
-
-        <Link
-          to="users"
-          className={`${isSidebarOpen ? "block" : "text-center"
-            } p-2 mb-4 text-gray-200 hover:bg-orange-400 rounded-lg group relative overflow-clip ${isActiveRoute("/users") ? "bg-orange-400 font-bold" : ""}`}
-        >
-          <span className="before:absolute before:content-[''] before:w-full before:h-[2px] before:bg-gray-200 before:bottom-2 before:-left-[100%] group-hover:before:left-[100%] group-hover:before:transition-all group-hover:before:duration-1000">
-            {isSidebarOpen ? "Users" : "U"}
-          </span>
-        </Link>
-
-        <button
-          onClick={handleLogout}
-          className={`${isSidebarOpen ? "block" : "text-center"
-            } p-2 bg-gray-100 text-gray-800 hover:bg-red-500 hover:text-gray-200 rounded-lg`}
-        >
-          {isSidebarOpen ? "Logout" : "L"}
-        </button>
-      </div>
-
-      <div className="flex-1 ml-16 md:ml-64 p-6">
-        <Outlet />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Dashboard;
